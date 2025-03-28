78cd2e5d00e41e4ee560c49ffc9df2833ba3daee
lauralopez20 <lauraguadalupelopez440@gmail.com>
2025-03-28 09:16:26 -0600

    Add files via upload

---
+++ Ejercicios_php/ejercicio 4.php
@@ -0,0 +1,19 @@
+<html>
+
+<head>
+	<title></title>
+</head>
+
+<body>
+	<h1>Primer ejemplo de variables</h1>
+
+<p>
+	<?php
+	$Name= "Miguel";
+	echo "Hola <b>$Name </b>, encantado de conocerte;"
+
+	?>
+</p>
+</body>
+
+</html>

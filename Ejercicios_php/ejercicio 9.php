f3150a9ad49c5ef90bcb4844da1ba0d8c3782cb4
lauralopez20 <lauraguadalupelopez440@gmail.com>
2025-03-28 09:20:27 -0600

    Add files via upload

---
+++ Ejercicios_php/ejercicio 9.php
@@ -0,0 +1,23 @@
+<html>
+
+<head>
+	<title> Introducción a los arrays, metodo corto </title>
+</head>
+
+<body>
+	<h1>Introducción a las arrays, metodo corto</h1>
+	<p> A continuación escribiremos los arrays de acuerdo al metodo corto </p>
+	<p>
+	<?php
+	/*
+	Este es el metodo corto para la creación de arrays, donde cada todo el array puede estar en una sola linea de codigo.
+	*/
+	$dia=array("Domingo","lunes","martes","miercoles","jueves","viernes","sabado");
+	//mostrar el miercoles
+
+	echo $dia[3];
+
+	?>	
+	</p>
+</body>
+</html>
